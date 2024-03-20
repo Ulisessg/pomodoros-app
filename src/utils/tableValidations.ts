0753ad118782194cbaf8d00ce67c572ce0a58581
Ulises <ukisessg@gmail.com>
2024-03-19 23:25:55 -0600

    Now table validations could have additional message

--- src/utils/tableValidations.ts
+++ src/utils/tableValidations.ts
@@ -1,35 +1,44 @@
 import { Tables } from "@/Types";
 
-export function idValidations({ id, table }: IdValidations) {
+export function idValidations({ id, table, extraMessage }: IdValidations) {
 	if (typeof id !== "number") {
-		throw new TypeError(`Id number type required. ${table}`);
+		throw new TypeError(`Id number type required. ${table} - ${extraMessage}`);
 	}
 	if (isNaN(id)) {
-		throw new Error(`Id NaN not allowed as id. ${table}`);
+		throw new Error(`Id NaN not allowed as id. ${table} - ${extraMessage}`);
 	}
 	if (id < 0) {
-		throw new Error(`Id invalid. ${table}`);
+		throw new Error(`Id invalid. ${table} - ${extraMessage}`);
 	}
 }
 
-export function nameValidations({ name, maxNameSize, table }: NameValidations) {
+export function nameValidations({
+	name,
+	maxNameSize,
+	table,
+	extraMessage,
+}: NameValidations) {
 	if (typeof name !== "string") {
-		throw new TypeError(`Name String type required ${table}`);
+		throw new TypeError(`Name String type required ${table} - ${extraMessage}`);
 	}
 	if (name.length === 0) {
-		throw new Error(`Name Username empty ${table}`);
+		throw new Error(`Name Username empty ${table} - ${extraMessage}`);
 	}
 	if (name.length > maxNameSize) {
-		throw new Error(`Name Username length more than 30 ${table}`);
+		throw new Error(
+			`Name Username length more than 30 ${table} - ${extraMessage}`
+		);
 	}
 }
 
 interface IdValidations {
 	id: number;
 	table: Tables;
+	extraMessage?: string;
 }
 interface NameValidations {
 	name: string;
 	maxNameSize: number;
 	table: Tables;
+	extraMessage?: string;
 }
