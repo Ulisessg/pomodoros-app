fd0d3b34b78a4022ea72272a10d932a76874a356
Ulises <ukisessg@gmail.com>
2024-04-01 21:10:52 -0600

    Output dir setted for production

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true
-  }
+  },
+  distDir: process.env.NODE_ENV === 'production' ? 'out': undefined
 };
 
 export default nextConfig;
