e4b5d8a82e95715a94d3a50f55003d1139af7cca
Ulises <ukisessg@gmail.com>
2024-04-03 00:19:18 -0600

    Base Path for production added

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
+const ENV = process.env.NODE_ENV
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true
   },
-  distDir: process.env.NODE_ENV === 'production' ? 'out': undefined
+  distDir: ENV === 'production' ? 'out': undefined,
+  basePath: ENV === 'production' ?  '/pomodoros': ''
 };
 
 export default nextConfig;
