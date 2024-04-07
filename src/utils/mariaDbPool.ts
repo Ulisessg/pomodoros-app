1f9343d8b7dc92ffff13ad69aa9defd5bf6ee79d
Ulises <ukisessg@gmail.com>
2024-04-06 20:55:03 -0600

    The database environment names were changed to more generic ones.

--- src/utils/mariaDbPool.ts
+++ src/utils/mariaDbPool.ts
@@ -1,10 +1,10 @@
 import mariadb from "mariadb";
 
 const mariaDbPool = mariadb.createPool({
-	host: process.env.MARIA_DB_HOST as string,
-	user: process.env.MARIA_DB_USER as string,
-	password: process.env.MARIA_DB_PASSWORD as string,
-	database: process.env.MARIA_DB_DATABASE as string,
+	host: process.env.DATA_DB_HOST as string,
+	user: process.env.DATA_DB_USER as string,
+	password: process.env.DATA_DB_PASSWORD as string,
+	database: process.env.DATA_DB_DATABASE as string,
 	connectionLimit: 5,
 });
 
