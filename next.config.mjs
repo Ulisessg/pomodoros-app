92f57e26a7802830cb07e6fcb5bce9fa56217091
Ulises <ukisessg@gmail.com>
2024-04-17 13:33:42 -0600

    Project version visible on header

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,17 @@
-const ENV = process.env.NODE_ENV
+import packageJson from "./package.json" assert { type: "json" };
+const ENV = process.env.NODE_ENV;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  compiler: {
-    styledComponents: true
-  },
-  distDir: ENV === 'production' ? 'out': undefined,
-  basePath: ENV === 'production' ?  '/pomodoros': ''
+	reactStrictMode: true,
+	compiler: {
+		styledComponents: true,
+	},
+	distDir: ENV === "production" ? "out" : undefined,
+	basePath: ENV === "production" ? "/pomodoros" : "",
+	env: {
+		NEXT_PUBLIC_VERSION: packageJson.version,
+	},
 };
 
 export default nextConfig;
--- src/components/atoms/Header.tsx
+++ src/components/atoms/Header.tsx
@@ -1,12 +1,24 @@
-import { Header, Link } from "d-system";
+import { Header, Link, theme } from "d-system";
 import React, { FC } from "react";
+import styled from "styled-components";
 
 const HeaderComponent: FC = () => {
 	return (
-		<Header>
+		<HeaderStyles>
 			<Link href="/" text="Home" />
-		</Header>
+			<p>Version: {process.env.NEXT_PUBLIC_VERSION}</p>
+		</HeaderStyles>
 	);
 };
+
+const HeaderStyles = styled(Header)`
+	align-items: center;
+	justify-content: space-between;
+	grid-auto-flow: column;
+	// Make version full visible
+	& p {
+		margin-right: ${theme.spacing * 3}px;
+	}
+`;
 
 export default HeaderComponent;
