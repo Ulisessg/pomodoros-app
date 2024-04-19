9e25b59e49b46d8c421a5b7ca3f174e5f7e8136c
Ulises <ukisessg@gmail.com>
2024-04-19 00:18:35 -0600

    Tomato image added in header

--- src/components/atoms/Header.tsx
+++ src/components/atoms/Header.tsx
@@ -1,11 +1,21 @@
-import { Header, Link, theme } from "d-system";
+import { StaticImagesPath } from "@/constants";
+import { Header, theme } from "d-system";
+import Image from "next/image";
+import Link from "next/link";
 import React, { FC } from "react";
 import styled from "styled-components";
 
 const HeaderComponent: FC = () => {
 	return (
 		<HeaderStyles>
-			<Link href="/" text="Home" />
+			<Link href="/" aria-label="Inicio">
+				<Image
+					alt="Tomato img"
+					src={`${StaticImagesPath}tomato-header.png`}
+					width={45}
+					height={40}
+				/>
+			</Link>
 			<p>Version: {process.env.NEXT_PUBLIC_VERSION}</p>
 		</HeaderStyles>
 	);
