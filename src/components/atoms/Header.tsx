18d78d8f69a9f6015349b233f98345cc7adae6b9
Ulises <ukisessg@gmail.com>
2024-04-19 11:34:36 -0600

    Replace Next Image component with HTML img element for the header image

--- src/components/atoms/Header.tsx
+++ src/components/atoms/Header.tsx
@@ -1,6 +1,5 @@
 import { StaticImagesPath } from "@/constants";
 import { Header, theme } from "d-system";
-import Image from "next/image";
 import Link from "next/link";
 import React, { FC } from "react";
 import styled from "styled-components";
@@ -9,11 +8,12 @@ const HeaderComponent: FC = () => {
 	return (
 		<HeaderStyles>
 			<Link href="/" aria-label="Inicio">
-				<Image
+				{/* eslint-disable-next-line @next/next/no-img-element */}
+				<img
 					alt="Tomato img"
 					src={`${StaticImagesPath}tomato-header.png`}
-					width={45}
-					height={40}
+					width={50}
+					height={45}
 				/>
 			</Link>
 			<p>Version: {process.env.NEXT_PUBLIC_VERSION}</p>
